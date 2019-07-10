ff0f939f88b1cac174c13cd9a15540da7370f4cb
Rodrigo Simões <rodrigofsimoes.pro@gmail.com>
2019-07-10 17:19:00 +0100

    use x-fmc nav data

--- airports.py
+++ airports.py
@@ -1,26 +1,44 @@
 import glob
 
 class Airports():
-    """ """
-    def find_airport(self, icao):
-        search_result = glob.glob("airac/SidStars/{}.txt".format(icao))
+    """"""
+
+    def find_airport_sid(self, icao):
+        search_result = glob.glob("airac/sids/{}.txt".format(icao))
 
         if len(search_result) > 0:
             return search_result[0]
         else:
             return None
     
+    def find_airport_star(self, icao):
+        search_result = glob.glob("airac/stars/{}.txt".format(icao))
+
+        if len(search_result) > 0:
+            return search_result[0]
+        else:
+            return None
+
     def open_airport_file(self, file_location):
-        file = open(file_location, 'r')
+        with open(file_location, 'r') as file:
+            return file.read()
 
-        return file
 
-    def get_file_section(self, file, section):
-        if section == 'SIDS':
-            return file.split(section)[1].split('ENDSIDS')[0]
+    def get_airport_sids(self, file):
+        return file.split('\n\n')
 
-    def parse_section_sids(self, sid_section):
-        return sid_section.split("\n")[1].split("\n")[0]
+    def parse_sid(self, sid_section):
+        sid_parsed = sid_section.split('\n')
+        return sid_parsed
 
-#with open('teste.txt', 'w') as file:
-#    file.write(Airports().parse_section_sids(Airports().get_file_section(Airports().open_airport_file(Airports().find_airport("LPPT")).read(), 'SIDS')))
+    def get_neighbours_from_airport(self, sid_parsed):
+        airport_sid = []
+        to_decode = sid_parsed[0].split('|')
+        sid = to_decode[3]
+        print("SID:", sid)
+        if sid in airport_sid:
+            pass
+        else:
+            airport_sid.append(sid)
+        print("List:", airport_sid)
+        return airport_sid
--- tests/test_airports.py
+++ tests/test_airports.py
@@ -2,9 +2,45 @@ from unittest import TestCase
 from airports import Airports
 
 class TestAiports(TestCase):
-    def test_airport_exists(self):
-        self.assertEqual(Airports().find_airport("LPPT"), 'airac/SidStars/LPPT.txt')
+    class Airport():
+        sid_location = Airports().find_airport_sid("LPPT")
+        star_location = Airports().find_airport_star("LPPT")
+        unkown_file_location = Airports().find_airport_sid("A0AA")
+        sid_file_to_read = None
+        #def __init__():
+        #    pass
 
-    def test_airport_none(self):
-        self.assertIsNone(Airports().find_airport("A0AA"))
+    airport = Airport()
+
+    def test_find_airport_sid_exists(self):
+        self.assertEqual(self.airport.sid_location, 'airac/sids/LPPT.txt')
+
+    def test_find_airport_star_exists(self):
+        self.assertEqual(self.airport.star_location, 'airac/stars/LPPT.txt')
+
+    def test_find_airport_none(self):
+        self.assertIsNone(self.airport.unkown_file_location)
+
+    def test_open_airport_file(self):
+        sid_file = Airports().open_airport_file(self.airport.sid_location)
+        self.assertIsNotNone(sid_file)
+
+    def test_get_airport_sids(self):
+        sid_file = Airports().open_airport_file(self.airport.sid_location)
+        self.assertIsNotNone(Airports().get_airport_sids(sid_file))
+
+    def test_parse_sid(self):
+        sid_file = Airports().open_airport_file(self.airport.sid_location)
+        sid_sections = Airports().get_airport_sids(sid_file)
+
+        for section in sid_sections:
+            self.assertIsNotNone(Airports().parse_sid(section))       
+    
+    def test_get_neighbours_from_airport(self):
+        sid_file = Airports().open_airport_file(self.airport.sid_location)
+        sid_sections = Airports().get_airport_sids(sid_file)
+        for section in sid_sections:
+            sid_parsed = Airports().parse_sid(section)
+            sid = Airports().get_neighbours_from_airport(sid_parsed)
+        #print(sid)
     
